f6d97d2449b828895d95270e93e7a6087add559c
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-11 13:18:11 +0300

    update

--- client/src/http/todo.ts
+++ client/src/http/todo.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AuthResponse } from '../modules/response/AuthResponse';
 
-export const API_URL = `http://localhost:8080/api`;
+export const API_URL = `https://versel-todo-jwt-server.vercel.app/api`;
 
 const $api = axios.create({
   withCredentials: true,
